1ec8e7e34a732b8e8c02c4c006825d3466cf5d87
ramart128 <ramzi.bensmira@yahoo.fr>
2014-05-05 19:12:32 +0100

    add a simple line

--- gStock/src/com/rmzb/gstock/Main.java
+++ gStock/src/com/rmzb/gstock/Main.java
@@ -3,7 +3,7 @@ package com.rmzb.gstock;
 public final class Main {
 
 	public static void main(String[] args) {
-		System.out.println("gStock");
+		System.out.println("gStock Version : 0.1");
 	}
 
 }
